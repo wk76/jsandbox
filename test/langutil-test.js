e7e896de7aed5a30593765760248218fbe5d492b
Andries <an3skrause@gmail.com>
2017-08-24 10:55:24 +0200

    Files updated with convertToMap utilities

--- test/langutil-test.js
+++ test/langutil-test.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 
+let convertUtil = require('../lib/lang');
+let users = require('./data/users.json');
+
+let results = convertUtil.convertToMap(users, "id", "report_log");
+console.log(results);
+
 
 
 
