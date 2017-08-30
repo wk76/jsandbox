3d69748826ece77d12f7452374ce70350a9b8e38
Andries <an3skrause@gmail.com>
2017-08-30 14:49:01 +0200

    Reporting functions implemented

--- app/reporter.js
+++ app/reporter.js
@@ -1,43 +1,71 @@
 
+let moment = require('moment');
 let langUtil = require('../lib/lang');
-
 let csvDataFile = "data/db.csv";
 let jsonDataFile = require('../data/db.json');
-
 let dateFormat = "YYYY-MM-DD";
 
 let reporter = {
 
-    getUsersForGivenBranch: function(branch) {
+    formatDate: function (arrayInput, dateFieldName) {
+        for (obj of arrayInput) {
+            let dateIn = obj[dateFieldName];
+            obj[dateFieldName] = moment(dateIn, ["DD-MM-YYYY", "YYYY-MM-DD", "DD/MM/YYYY", "YYYY/MM/DD"], true).format(dateFormat);
+        }
+        return arrayInput;
+    },
 
+    filterOn: function (filterLabel, filterValue) {
         let users = [];
+        for (user of this.formatDate(jsonDataFile, "DATE OF EMPLOYMENT")) {
+            if (user[filterLabel] === filterValue)
+                users.push(user);
+        }
+        return users;
+    },
 
-        for (user of jsonDataFile) {
-            console.log(user);
-            users.push(user);
+    getUsersForGivenBranch: function (branch) {
+        let reportOut = this.filterOn("BRANCH", branch);
+        return reportOut;
+    },
+
+    getUsersForGivenSupervisor: function (supervisor) {
+        let reportOut = this.filterOn("SUPERVISOR", supervisor);
+        return reportOut;
+    },
+
+    getUserCountRE5ObtainedForGivenBranch: function (branch) {
+        let userCount = 0, branchUsers = this.filterOn("BRANCH", branch)
+        for (i of branchUsers) {
+            if (i["RE (5)"] === "YES")
+                userCount++;
+        }
+        return userCount;
+    },
+
+    getUserCountRE5NotObtainedForGivenBranch: function (branch) {
+        let userCount = 0, branchUsers = this.filterOn("BRANCH", branch)
+        for (i of branchUsers) {
+            if (i["RE (5)"] !== "YES")
+                userCount++;
+        }
+        return userCount;
+    },
+
+    getUsersEmployedBeforeDate: function (date) {
+        let users = [];
+        for (user of this.formatDate(jsonDataFile, "DATE OF EMPLOYMENT")) {
+            let getValidDate = moment(user["DATE OF EMPLOYMENT"], "YYYY-MM-DD");
+            if (getValidDate.isValid()) {
+                if (moment(user["DATE OF EMPLOYMENT"]) < moment(date))
+                    users.push(user);
+            }
         }
 
         return users;
-
-    },
-
-    getUsersForGivenSupervisor: function(supervisor) {
-
-    },
-
-    getUserCountRE5ObtainedForGivenBranch: function(branch) {
-
-    },
-
-    getUserCountRE5NotObtainedForGivenBranch: function(branch) {
-        
-    },
-
-    getUsersEmployedBefore2014: function() {
-
     }
-
 }
+
 
 exports.reporter = reporter;
 
--- app/run-report.js
+++ app/run-report.js
@@ -1,18 +1,39 @@
 #!/usr/bin/env node
 
+let moment = require('moment');
 let reporter = require('./reporter').reporter;
 
-let printUsers = function(users, branch) {
-    branch ? console.log('------------ ' + branch + ' ------------') : '';
-    for (user of users) {
-        console.log(user);
+
+
+let printResults = function (resultSet, heading) {
+    let headerpadding = '-------------------------------------------------------------------'
+    heading ? console.log(headerpadding.substring(1,5) + ' ' + heading + ' ' + headerpadding.substring(1, headerpadding.length - heading.length )) : '';
+    if (Array.isArray(resultSet)) {
+        for (result of resultSet)
+        { console.log(result); }
+    } else {
+        console.log(resultSet);
     }
-    branch ? console.log(' ') : '';
-}
+    heading ? console.log(' ') : '';
+};
+
+
 
 let durbanUsers = reporter.getUsersForGivenBranch('Atron Durban');
-printUsers(durbanUsers, "DURBAN");
+printResults(durbanUsers, "USERS FOR DURBAN");
 
 let gautengUsers = reporter.getUsersForGivenBranch('Gauteng');
-printUsers(durbanUsers, "GaUTENG");
+printResults(gautengUsers, "USERS FOR GAUTENG");
+
+let supervisorWarrenAllanUsers = reporter.getUsersForGivenSupervisor('Warren Allan');
+printResults(supervisorWarrenAllanUsers, "USERS FOR SUPERVISOR WARREN ALLAN");
+
+let totalGautengUsersRE5Obtained = reporter.getUserCountRE5ObtainedForGivenBranch('Gauteng');
+printResults(totalGautengUsersRE5Obtained, "TOTAL GAUTENG USERS WITH RE5 OBTAINED");
+
+let totalGautengUsersRE5NotObtained = reporter.getUserCountRE5NotObtainedForGivenBranch('Gauteng');
+printResults(totalGautengUsersRE5NotObtained, "TOTAL GAUTENG USERS WITH RE5 OUTSTANDING");
+
+let usersEmployedBefore2014 = reporter.getUsersEmployedBeforeDate('2014-01-02');
+printResults(usersEmployedBefore2014, "USERS EMPLOYED BEFORE 2014");
 
